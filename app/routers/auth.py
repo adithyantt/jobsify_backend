c15b95f02194c84d6ec61e50f2857c0450668fb3
Adithyan T T <adithyantt00@gmail.com>
2025-12-26 15:54:12 +0530

    this is the updated version of the auth database

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter,Depends,
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models.user import User
 
-router = APIRouter
+router = APIRouter()
 
-#Dataabse dependency
+# Database dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -13,24 +13,22 @@ def get_db():
     finally:
         db.close()
 
-        @router.post("/register")
-        def register_user(
-            name: str,
-            email: str,
-            password: str,
-            role: str,
-                   db: Session = Depends(get_db)
-        ):
-            
-            user = User(
-                name=name,
-                email=email,
-                password=password,
-                role=role   
-            
-            )
-    
-db.add(user)
-db.commit()
-db.refresh(user)
-return {"message" :"User regiistered successfully"}
+@router.post("/register")
+def register_user(
+    name: str,
+    email: str,
+    password: str,
+    role: str,
+    db: Session = Depends(get_db)
+):
+    user = User(
+        name=name,
+        email=email,
+        password=password,
+        role=role
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return {"message": "User registered successfully"}
