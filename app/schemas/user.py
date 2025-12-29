9079324fa4c0a324703d5874d4e520db165b0602
Adithyan T T <adithyantt00@gmail.com>
2025-12-29 23:27:16 +0530

    updating schemes file

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel, EmailStr
 
+# ✅ For Registration
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
     password: str
     role: str
+
+
+# ✅ For Login
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
