f155cd1fc1e6e08d4b278f56ae6ab9122d90bacc
Adithyan T T <adithyantt00@gmail.com>
2026-01-25 21:48:19 +0530

    updating

--- app/routers/jobs.py
+++ app/routers/jobs.py
@@ -1,18 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-router = APIRouter()
+from app.database import get_db
+from app.models.job import Job
+from app.schemas.job import JobCreate, JobResponse
 
-@router.get("/jobs")
-def get_jobs():
-    return [
-        {
-            "title": "Electrician needed",
-            "category": "Electrician",
-            "location": "Thiruvanthapuram"
-        },
-        {
-        "title": "Plumber Required",
-        "category" : "Plumber",
-        "location": "Kollam"
-        }
-    ]
+router = APIRouter(tags=["Jobs"])
+
+
+# ✅ GET ALL JOBS
+@router.get("/jobs", response_model=list[JobResponse])
+def get_jobs(db: Session = Depends(get_db)):
+    return db.query(Job).all()
+
+
+# ✅ CREATE JOB
+@router.post("/jobs", response_model=JobResponse)
+def create_job(job: JobCreate, db: Session = Depends(get_db)):
+    new_job = Job(
+        title=job.title,
+        category=job.category,
+        description=job.description,
+        location=job.location,
+    )
+
+    db.add(new_job)
+    db.commit()
+    db.refresh(new_job)
+
+    return new_job
